ddaa01f7d584047b0c648e5280f9d133a711d83c
rogeriopvl <rogeriopvl@gmail.com>
2013-11-06 22:35:37 +0000

    Added more tests to metadata. All passing.

--- test/meocloud_test.js
+++ test/meocloud_test.js
@@ -1,9 +1,14 @@
 var expect = require('chai').expect;
 var nock = require('nock');
+var querystring = require('querystring');
 
 var meocloud = require('../lib/meocloud.js')({});
 
 describe('MEOCloud', function() {
+
+    describe('OAuth', function() {
+        // TODO
+    });
 
     describe('Metadata', function() {
         it('should exist as a plublic method on MEOCloud', function(done) {
@@ -11,11 +16,35 @@ describe('MEOCloud', function() {
             done();
         });
 
-        it('should make correct metadata request', function(done) {
+        it('should make correct metadata request without params', function(done) {
             nock('https://api.meocloud.pt').get('/1/Metadata/meocloud/test')
             .reply(200, { is_dir: true, root: 'meocloud' });
 
             meocloud.metadata('/test', {}, function(err, data) {
+                expect(err).to.not.be.ok;
+                expect(data).to.be.an('object');
+                expect(data).to.have.ownProperty('is_dir');
+                expect(data).to.have.ownProperty('root');
+                done();
+            });
+        });
+
+        it('should make correct metadata request with params', function(done) {
+            var query = {
+                file_limit: 10,
+                hash: 'blabla',
+                list: false,
+                include_deleted: true,
+                rev: 'foobar'
+            };
+
+            var qs = querystring.stringify(query);
+
+            nock('https://api.meocloud.pt')
+            .get('/1/Metadata/meocloud/test?' + qs)
+            .reply(200, { is_dir: true, root: 'meocloud' });
+
+            meocloud.metadata('/test', query, function(err, data) {
                 expect(err).to.not.be.ok;
                 expect(data).to.be.an('object');
                 expect(data).to.have.ownProperty('is_dir');
