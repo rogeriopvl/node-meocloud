693004b2c3e18e59aa6dc9a0936c0c155a34ec88
rogeriopvl <rogeriopvl@gmail.com>
2013-11-11 22:04:13 +0000

    Add metadataShare with passing tests

--- test/meocloud_test.js
+++ test/meocloud_test.js
@@ -78,4 +78,37 @@ describe('MEOCloud', function() {
             });
         });
     });
+
+    describe('MetadataShare', function() {
+
+        beforeEach(function(done) {
+            meocloud = new MEOCloud({});
+            done();
+        });
+
+        it('should exist as a plublic method on MEOCloud', function(done) {
+            expect(typeof meocloud.metadataShare).to.equal('function');
+            done();
+        });
+
+
+        it('should make correct metadataShare request without params', function(done) {
+            nock('https://api.meocloud.pt')
+            .get('/1/MetadataShare/bb26f0f6-d66f-46a7-8905-1fc125a293e7/test')
+            .reply(200, { is_dir: true, root: 'meocloud' });
+
+            meocloud.metadataShare(
+                'bb26f0f6-d66f-46a7-8905-1fc125a293e7',
+                '/test',
+                null,
+                function(err, data) {
+                    expect(err).to.not.be.ok;
+                    expect(data).to.be.an('object');
+                    expect(data).to.have.ownProperty('is_dir');
+                    expect(data).to.have.ownProperty('root');
+                    done();
+                }
+            );
+        });
+    });
 });
